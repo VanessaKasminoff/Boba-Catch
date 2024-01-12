9f892fbb24a4b83c2b70a7fb88a6792816e95d45
Vanessa Kasminoff <sunnset07@gmail.com>
2024-01-11 21:34:38 -0800

    added proper dynamic adjustable catch mechanism

--- assets/js/loadGame.js
+++ assets/js/loadGame.js
@@ -1,6 +1,6 @@
 import settings from '../data/settings.json' assert { type: 'json' };
-let cupWidth = settings.game.cupSizePercent
-let gameArea // if this value was returned in resizeGameArea, then it would only be returned for the instance it was invoked. This is problematic because we need this value to be changed on each resize.
+let cupWidth = settings.game.cupSizePercentOfWidth
+let dynamicGameArea // if this value was returned in resizeGameArea, then it would only be returned for the instance it was invoked. This is problematic because we need this value to be changed on each resize.
 
 export function resizeGameArea(container) {
     let solutionWidth
@@ -12,21 +12,36 @@ export function resizeGameArea(container) {
     container.style.height = (`${solutionWidth * 2}px`)
     container.style.width = (`${solutionWidth}px`)
     container.style.top = (`${window.innerHeight * 0.02}px`)
-    gameArea =  {
+    dynamicGameArea =  {
         width: solutionWidth,
         height: solutionWidth*2
         }
      
 }
 
+function createTable() {
+    let table = document.createElement('div')
+    table.id = "table"
+    table.style = `
+    width: 100%;
+    height: ${settings.game.tableHeightPercentOfHeight}%;
+    background: brown;
+    position: absolute;
+    bottom: ${0}px
+    `
+
+    return table
+}
+
 function createCup() {
     let cup = document.createElement("div")
     cup.id = "cup"
     cup.style = `
-    width: ${cupWidth * 2}%;
-    height: ${cupWidth}%;
+    width: ${cupWidth}%;
+    height: ${settings.game.cupHeightPercentOfHeight}%;
     background: darkblue;
     position: absolute;
+    bottom: ${settings.game.cupHeightByPercentOfHeight}%
     `
 
     return cup
@@ -40,12 +55,18 @@ function attachCupMouseMovement(cup, gameContainer) {
         //mouse coords relative to the container = mouse absolute coordinates - containers relative position.
         const mouseX = e.clientX - gameContainerPos.left;
         //updates the cup's position.
-        let halfCupWidth = cupWidth * 0.01 * gameArea.width
+        let halfCupWidth = (cupWidth * 0.01 * dynamicGameArea.width)/2
+        let halfBobaWidth = dynamicGameArea.width * settings.game.bobaSizePercentOfWidth * .01  / 2
+        //console.log(halfBobaWidth)
+        //console.log(halfCupWidth)
+
         cup.style.left = (mouseX - halfCupWidth)+ 'px';
+        cup.leftBound = mouseX - halfCupWidth - halfBobaWidth
+        cup.rightBound = mouseX + halfCupWidth - halfBobaWidth
 
         //sets static position of cup to stop it from moving past the right side of the container.
-        if (mouseX >= gameArea.width * 0.9 + halfCupWidth) {
-            cup.style.left = (gameArea.width * 0.9)+ 'px'
+        if (mouseX >= dynamicGameArea.width * 0.9 + halfCupWidth) {
+            cup.style.left = (dynamicGameArea.width * 0.9)+ 'px'
         }
         if (mouseX <= halfCupWidth){
             cup.style.left = 0 + "px"
@@ -56,40 +77,66 @@ function attachCupMouseMovement(cup, gameContainer) {
 
 export function loadGame(gameContainer) {
     resizeGameArea(gameContainer)
+    gameContainer.append(createTable())
+
+    /* you COUUULD do 
+    gameContainer.append(attachCupMouseMovement(createCup(), gameContainer))
+    and return cup from attachCupMouseMovement... But that's unreadable to the human eye.*/
     let cup = createCup()
     gameContainer.append(cup)
     attachCupMouseMovement(cup, gameContainer)
 
 // GENERATE FALLING BOBA
+    function createBoba() {
+        let bobaSizePercent = settings.game.bobaSizePercentOfWidth
+        let bobaSize = (bobaSizePercent * .01 * dynamicGameArea.width) // for readability
+        let bobaHeight = dynamicGameArea.height - bobaSize;
+        let bobaX = Math.random() * (dynamicGameArea.width - bobaSize);
+        const boba = document.createElement('div');
+        boba.style = `
+        width: ${bobaSizePercent}%;
+        height: ${bobaSizePercent / 2}%;
+        border-radius: 50%;
+        background: black;
+        position: absolute;
+        `
+        gameContainer.append(boba)
 
-function createBoba() {
-    let bobaSize = settings.game.bobaSizePercent
-    let bobaStart = gameArea.height - bobaSize;
-    let bobaX = Math.floor(Math.random() * gameArea.width);
-    const boba = document.createElement('div');
-    boba.setAttribute("class", 'boba');
-    boba.style = `
-    width: ${bobaSize}%;
-    height: ${bobaSize / 2}%;
-    border-radius: 50%;
-    background: black;
-    position: absolute;
-    `
-    gameContainer.append(boba)
+        let uncaught = true
+        function fallingBoba() {
+            bobaHeight -= 1;
+            boba.style.bottom = bobaHeight + 'px';
+            boba.style.left = bobaX + 'px'; 
+            
+            //catching
 
-    function fallingBoba() {
-        bobaStart -= 8;
-        boba.style.bottom = bobaStart + 'px';
-        boba.style.left = bobaX + 'px';
-        if(bobaStart < (0.1 * gameArea.height)) {
-            boba.remove();
+            let cupRimHeight = ((settings.game.cupHeightByPercentOfHeight + settings.game.cupHeightPercentOfHeight -settings.game.bobaSizePercentOfWidth/4) * .01 * dynamicGameArea.height)
+            let halfCupHeight = cupRimHeight - (settings.game.cupHeightPercentOfHeight * dynamicGameArea.width  *.01 )
+            let tableHeight = ((settings.game.tableHeightPercentOfHeight - settings.game.bobaSizePercentOfWidth) * dynamicGameArea.height * .01)
+            //console.log(cupRimHeight)
+            //console.log(halfCupHeight)
+            if(uncaught && bobaHeight > halfCupHeight && bobaHeight < cupRimHeight) {
+                if((bobaX > cup.leftBound) && (bobaX < cup.rightBound)){
+                    uncaught = false
+                    console.log("caught")
+                    boba.remove()
+                    }
+            }
+            //removal
+            if(uncaught && bobaHeight < tableHeight) {
+                boba.remove()
+                
+                uncaught = false
+                console.log("YOU SUCCCCK")
+            }
+
+        //console.log("this is the falling boba function")
         }
+
+    setInterval(fallingBoba, 1);
+    setTimeout(createBoba, 1200);
+
     }
-    setInterval(fallingBoba, 30);
-    setTimeout(createBoba, 1100);
-
-}
-
 
 
 createBoba();
